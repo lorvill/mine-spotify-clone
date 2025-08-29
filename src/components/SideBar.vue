d1a550d45fe04181233aafa1be57da6f5579ac1f
Sabrina Bicheva <aprilfrommay444@gmail.com>
2025-08-29 16:41:43 +0300

    first commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="SideNav" class="h-[100%] p-6 w-[240px] fixed z-50 bg-black">
+    <div class="my-8"></div>
+    <div class="border-b border-b-gray-700"></div>
+    <ul>
+      <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #1</li>
+      <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #2</li>
+      <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #3</li>
+      <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #4</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import SideBar from '@/components/SideBar.vue';
+import ContentArea from '@/components/ContentArea.vue';
+
+
+</script>
+
+<template>
+
+  <SideBar />
+  <ContentArea />
+</template>
+
+<style scoped></style>
